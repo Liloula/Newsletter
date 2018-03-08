7be2e59926255e6a6a075a148e604c252a2071cb
Unknown <eloise.hall@sfr.fr>
2018-03-08 15:56:25 +0100

    ajout

--- ressources/js/script.js
+++ ressources/js/script.js
@@ -1,23 +1,17 @@
-.waypoint(function(direction){
-  if (direction=="down"){
-    $("nav").addClass("sticky");
-  }else{
-    $("nav").removeClass("sticky");
-  }
-})
-
-
-
-
-
-/*$(document).ready(function() {
-$("h1").click(function () {
-$(this).css("background-color", "#ff0000");
-})
-});
-
 $(document).ready(function() {
-$("h2").click(function () {
-$(this).css("background-color", "#ff0000");
-})
-});
+
+  $("h1").click(function () {
+    $(this).css("background-color", "#ff0000");
+  })
+
+    $(".js--waypoint-photos").waypoint(function(direction) {
+      if (direction=="down"){
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    })
+
+    $(".js-scroll-photos").click(function() {
+      $("html, body").animate({scrollTop: $(".js-section-photos").offset().top});
+    })
