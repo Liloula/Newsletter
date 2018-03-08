f9b93854a7df7839c9a6e4230cebe5332bf34de6
Unknown <eloise.hall@sfr.fr>
2018-03-08 15:08:13 +0100

    ajout 1 effet

---
+++ ressources/js/script.js
@@ -0,0 +1,23 @@
+.waypoint(function(direction){
+  if (direction=="down"){
+    $("nav").addClass("sticky");
+  }else{
+    $("nav").removeClass("sticky");
+  }
+})
+
+
+
+
+
+/*$(document).ready(function() {
+$("h1").click(function () {
+$(this).css("background-color", "#ff0000");
+})
+});
+
+$(document).ready(function() {
+$("h2").click(function () {
+$(this).css("background-color", "#ff0000");
+})
+});
